97d388c0a0de16d18616897831c78dbaf2f664a5
hujiaodou96 <37376115+hujiaodou96@users.noreply.github.com>
2020-09-14 14:30:05 +0200

    Update __init__.py

--- trajnetbaselines/lstm/__init__.py
+++ trajnetbaselines/lstm/__init__.py
@@ -1,5 +1,6 @@
 from .loss import PredictionLoss, L2Loss
-from .lstm import LSTM, LSTMPredictor
+#from .lstm import LSTM, LSTMPredictor
+from .lstm_update import LSTM, LSTMPredictor
 from .gridbased_pooling import GridBasedPooling
 from .non_gridbased_pooling import NN_Pooling, HiddenStateMLPPooling, AttentionMLPPooling, DirectionalMLPPooling
 from .non_gridbased_pooling import NN_LSTM, TrajectronPooling, SAttention_fast
